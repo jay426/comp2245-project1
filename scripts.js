5824449c14627f90b3ee063f8ff9b4c2971ede7d
Javeesha Burton <javeesha14@gmail.com>
2024-11-06 22:33:32 -0500

    Updated files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+document.getElementById('newsletter-form').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+  
+    const emailInput = document.getElementById('email-input');
+    const email = emailInput.value.trim();
+  
+    const messageDiv = document.querySelector('.message');
+  
+    if (email) {
+      
+      messageDiv.textContent = `Thank you! Your email address "${email}" has been added to our mailing list!`;
+      
+    } else {
+
+      messageDiv.textContent = 'Please enter a valid email address.';
+    }
+  
+    emailInput.value = '';
+  });
